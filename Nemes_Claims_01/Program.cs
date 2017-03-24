125889d94e1481839880d62dc9f8483578b93647
Nick Campanini <necampanini@gmail.com>
2017-03-23 20:25:59 -0400

    start of claims study

--- Nemes_Claims_01/Program.cs
+++ Nemes_Claims_01/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Nemes_Claims_01
 {
@@ -10,6 +8,29 @@ namespace Nemes_Claims_01
     {
         static void Main(string[] args)
         {
+            Claim claim = new Claim("Name", "Nicky");
+            //strings bad, there are standard claim types
+
+            Claim newClaim = new Claim(ClaimTypes.Country, "USA");
+
+            //#AAA111 -- note identifier :)
+            IList<Claim> claimCollection = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Nicky"),
+                new Claim(ClaimTypes.Country, "USA"),
+                new Claim(ClaimTypes.Gender, "M"),
+                new Claim(ClaimTypes.Surname, "Campanini"),
+                new Claim(ClaimTypes.Email, "necampanini@gmail.com"),
+                new Claim(ClaimTypes.Role, "Software Engineer")
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claimCollection);
+
+            var authenticatedClaimsIdentity = new ClaimsIdentity(
+                claimCollection, "my pretend e-commerce website");
+            Console.WriteLine("claims identity is authenticated: " + claimsIdentity.IsAuthenticated);
+            Console.WriteLine("auth'd claims Identity is authenticated: " + authenticatedClaimsIdentity.IsAuthenticated);
+            Console.ReadKey();
         }
     }
 }
